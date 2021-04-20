832b3778f1392860b3e491ae91e8485ff6ddde8f
ppa21 <pruthviraj_patel@sfu.ca>
2021-04-19 19:02:51 -0700

    Added solution to Add Two Numbers

---
+++ src/2_AddTwoNumbers/Solution.java
@@ -0,0 +1,61 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        // starting variables
+        int v1 = 0;
+        int v2 = 0;
+        int carry = 0;
+        int sum = 0;
+        ListNode result = new ListNode();
+        ListNode tempResult = result;
+
+        while(l1 != null || l2 != null) {
+            /* if l1 is not null, extract the value from l1 and move the pointer for l1 to the next element;
+             * otherwise, v1 is 0
+             */
+            if(l1 != null) {
+                v1 = l1.val;
+                l1 = l1.next;
+            } else {
+                v1 = 0;
+            }
+
+            /* if l2 is not null, extract the value from l2 and move the pointer for l2 to the next element;
+             * otherwise, v2 is 0
+             */
+            if(l2 != null) {
+                v2 = l2.val;
+                l2 = l2.next;
+            } else {
+                v2 = 0;
+            }
+
+            sum = v1 + v2 + carry;
+
+            // sum = 12; carry = 12 / 10 -> 1; sum = 12 % 10 -> 2;
+            carry = sum / 10;
+            sum = sum % 10;
+
+            // sum = 2;
+            ListNode temp = new ListNode(sum);
+            tempResult.next = temp;
+            tempResult = temp;
+        }
+
+        if(carry != 0) {
+            ListNode temp = new ListNode(carry);
+            tempResult.next = temp;
+        }
+
+        return result.next;
+    }
+}
