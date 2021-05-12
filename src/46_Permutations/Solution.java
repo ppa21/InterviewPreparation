92435a5b8e03bdb49c7e1882da368836221a998f
ppa21 <pruthviraj_patel@sfu.ca>
2021-05-11 18:26:02 -0700

    Added a solution to Permutations question and a comment about Backtracking

---
+++ src/46_Permutations/Solution.java
@@ -0,0 +1,31 @@
+/*
+    Backtracking problem
+        * Use the Backtracking template from my bookmarks
+            * Solution is similar to question 17
+ */
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        backtrack(nums, new ArrayList<>(), new boolean[nums.length], result);
+
+        return result;
+    }
+
+    private void backtrack(int[] nums, List<Integer> permutations, boolean[] visited, List<List<Integer>> result) {
+        if(nums.length == permutations.size()) {
+            result.add(new ArrayList<>(permutations));
+            return;
+        }
+
+        for(int i = 0; i < nums.length; i++) {
+            if(visited[i] == false) {
+                visited[i] = true;
+                permutations.add(nums[i]);
+                backtrack(nums, permutations, visited, result);
+                visited[i] = false;
+                permutations.remove(permutations.size() - 1);
+            }
+        }
+    }
+}
