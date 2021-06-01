0d9fb62c2f2abc19f74d95bb6f1dd271a2465e8f
ppa21 <pruthviraj_patel@sfu.ca>
2021-05-31 18:26:29 -0700

    Added solution two to Longest Increasing Subsequence question

---
+++ src/300_LongestIncreasingSubsequence/SolutionTwo.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+
+        int result = 0;
+
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = 0; j < i; j++) {
+                if(nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], 1 + dp[j]);
+                }
+            }
+
+            result = Math.max(result, dp[i]);
+        }
+
+        return result;
+    }
+}
