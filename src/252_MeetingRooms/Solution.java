cd38bb7f138806fbc56af57452c0ddae928553b5
ppa21 <pruthviraj_patel@sfu.ca>
2021-06-20 18:48:35 -0700

    Added a solution to Meeting Rooms problem

---
+++ src/252_MeetingRooms/Solution.java
@@ -0,0 +1,37 @@
+/**
+ * Definition of Interval:
+ * public classs Interval {
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param intervals: an array of meeting time intervals
+     * @return: if a person could attend all meetings
+     */
+    public boolean canAttendMeetings(List<Interval> intervals) {
+        // Write your code here
+
+        Collections.sort(intervals, new Comparator<Interval>() {
+            public int compare(Interval a, Interval b) {
+                return a.start - b.start;
+            }
+        });
+
+        for(int i = 1; i < intervals.size(); i++) {
+            Interval last = intervals.get(i - 1);
+            Interval curr = intervals.get(i);
+
+            if(last.end > curr.start) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
