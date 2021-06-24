fdeb1845dd1305156ff0c0ec98c239f35cfbaf7b
ppa21 <pruthviraj_patel@sfu.ca>
2021-06-24 16:37:38 -0700

    Added a solution to Longest Repeating Character Replacement question

---
+++ src/424_LongestRepeatingCharacterReplacement/Solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        Map<Character, Integer> count = new HashMap();
+        int result = 0;
+        int left = 0;
+
+        for(int right = 0; right < s.length(); right++) {
+            count.put(s.charAt(right), count.getOrDefault(s.charAt(right), 0) + 1);
+
+            /*
+                    * windowSize = right - left + 1
+                        * right and left = indexes in s
+                            * s is zero-based index
+                                * so add 1 to right - left to get windowSize
+                                    * windowSize = right - left + 1
+             */
+            if((right - left + 1) - Collections.max(count.values()) > k) {
+                count.put(s.charAt(left), count.getOrDefault(s.charAt(left), 0) - 1);
+                left++;
+            }
+
+            result = Math.max(result, right - left + 1);
+        }
+
+        return result;
+    }
+}
