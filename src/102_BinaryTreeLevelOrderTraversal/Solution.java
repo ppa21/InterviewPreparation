9c9a92889c660505ee0df81a5d9d404ae41e861c
ppa21 <pruthviraj_patel@sfu.ca>
2021-07-15 15:57:13 -0700

    Added a solution to Binary Tree Level Order Traversal question with Time and Space Complexity

---
+++ src/102_BinaryTreeLevelOrderTraversal/Solution.java
@@ -0,0 +1,59 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    /*
+            * Uses BFS
+                * Serves as a TEMPLATE for BFS
+     */
+
+    /*
+            * Time Complexity:      O(N) ---> N = number of nodes in the tree
+            * Space Complexity:     O(N) ---> N = number of nodes in the tree
+     */
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList();
+
+        if(root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList();
+        queue.add(root);
+
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> currentLevel = new ArrayList();
+
+            for(int i = 0; i < size; i++) {
+                TreeNode current = queue.remove();
+                currentLevel.add(current.val);
+
+                if(current.left != null) {
+                    queue.add(current.left);
+                }
+
+                if(current.right != null) {
+                    queue.add(current.right);
+                }
+            }
+
+            result.add(currentLevel);
+        }
+
+        return result;
+    }
+}
