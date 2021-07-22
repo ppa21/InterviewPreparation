15fac30466738f3c5c19fdc8517b92fd2a4b8171
ppa21 <pruthviraj_patel@sfu.ca>
2021-07-22 16:33:02 -0700

    Added a solution to Serialize and Deserialize Binary Tree question

---
+++ src/297_SerializeandDeserializeBinaryTree/Solution.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Codec {
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        if(root == null) {
+            return "null";
+        }
+
+        return root.val + "," + serialize(root.left) + "," + serialize(root.right);
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        Queue<String> queue = new LinkedList(Arrays.asList(data.split(",")));
+
+        return dfs(queue);
+    }
+
+    private TreeNode dfs(Queue<String> queue) {
+        String s = queue.remove();
+
+        if(s.equals("null")) {
+            return null;
+        }
+
+        TreeNode node = new TreeNode(Integer.valueOf(s));
+        node.left = dfs(queue);
+        node.right = dfs(queue);
+
+        return node;
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec ser = new Codec();
+// Codec deser = new Codec();
+// TreeNode ans = deser.deserialize(ser.serialize(root));
