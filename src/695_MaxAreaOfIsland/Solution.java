653e22dc2e8a3f558d10c62526eae20ca48ede8e
ppa21 <47469086+ppa21@users.noreply.github.com>
2024-06-17 21:56:36 -0700

    Added a solution to Max Area of Island with Time and Space Complexity

---
+++ src/695_MaxAreaOfIsland/Solution.java
@@ -0,0 +1,32 @@
+class Solution {
+    /*
+        * SIMILAR PROBLEM TO: 
+            * 200. Number of Islands
+
+        * Time Complexity  = O(nm); n = grid.length; m = grid[i].length; AKA size of matrix
+        * Space Complexity = O(nm)
+    */
+    public int maxAreaOfIsland(int[][] grid) {
+        int max = 0;
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == 1) {
+                    max = Math.max(max, dfs(grid, i, j));
+                }
+            }
+        }
+
+        return max;
+    }
+
+    private int dfs(int[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] == 0) {
+            return 0;
+        }
+
+        grid[i][j] = 0;
+
+        return 1 + dfs(grid, i - 1, j) + dfs(grid, i + 1, j) + dfs(grid, i, j - 1) + dfs(grid, i, j + 1);
+    }
+}
