83e7cf967b326a2fe8047f56af19c9743b263437
ppa21 <47469086+ppa21@users.noreply.github.com>
2024-06-18 22:02:59 -0700

    Added a solution to Surrounded Regions with Time and Space Complexity

---
+++ src/130_SurroundedRegions/Solution.java
@@ -0,0 +1,59 @@
+class Solution {
+    /*
+        * Time Complexity  = O(nm)
+        * Space Complexity = O(nm)
+    */
+    public void solve(char[][] board) {
+        int rows = board.length;
+        int cols = board[0].length;
+
+        // 1a) Capture unsurrounded regions AKA border 'O's; top and bottom row (O -> T)
+        for (int i = 0; i < cols; i++) {
+            if (board[0][i] == 'O') {
+                dfs(board, 0, i);
+            }
+
+            if (board[rows - 1][i] == 'O') {
+                dfs(board, rows - 1, i);
+            }
+        }
+
+        // 1b) Capture unsurrounded regions AKA border 'O's; left and right cols (O -> T)
+        for (int i = 0; i < rows; i++) {
+            if (board[i][0] == 'O') {
+                dfs(board, i, 0);
+            }
+
+            if (board[i][cols - 1] == 'O') {
+                dfs(board, i, cols - 1);
+            }
+        }
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                // 2) capture surrounded regions (O -> X)
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }
+
+                // 3) uncapture unsurrounded regions (T -> O)
+                if (board[i][j] == 'T') {
+                    board[i][j] = 'O';
+                }
+            }
+        }
+    }
+
+    private void dfs(char[][] board, int i, int j) {
+        if (i < 0 || i >= board.length || j < 0 || j >= board[i].length || board[i][j] != 'O') {
+            return;
+        }
+
+        board[i][j] = 'T';
+
+        dfs(board, i - 1, j);
+        dfs(board, i + 1, j);
+        dfs(board, i, j - 1);
+        dfs(board, i, j + 1);
+    }
+}
