d6fe19af98532edf425210b272625cbf824e7f1f
ppa21 <47469086+ppa21@users.noreply.github.com>
2024-06-24 19:38:26 -0700

    Added a solution to Min Cost to Connect All Points with Time and Space Complexity

---
+++ src/1584_MinCosttoConnectAllPoints/Solution.java
@@ -0,0 +1,41 @@
+class Solution {
+    /* 
+        * Prim's Algorithm
+        
+        * Time Complexity  = O(N^2 log(N)); N = length of points; N^2 = finding distance between a currNode and every other node to pick the shortest distance; log(N) = Priority Queue
+        * Space Complexity = O(N^2)
+    */
+    public int minCostConnectPoints(int[][] points) {
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> a[0] - b[0]); // a = edge weight; b = index of the current node
+        minHeap.offer(new int[] { 0, 0 });
+
+        Set<Integer> visited = new HashSet<>(); // so we don't visit a node twice (THIS CREATES A LOOP)
+        int cost = 0;
+
+        // When visited.size() == points.length meaning that all the nodes has been connected.
+        while (visited.size() < points.length) {
+            int[] arr = minHeap.poll();
+
+            int weight = arr[0];
+            int currNode = arr[1];
+
+            if (visited.contains(currNode)) {
+                continue;
+            }
+
+            visited.add(currNode);
+            cost += weight;
+
+            // visiting neighboring nodes in the frontier from currNode
+            for (int nextNode = 0; nextNode < points.length; nextNode++) {
+                if (!visited.contains(nextNode)) {
+                    int nextWeight = Math.abs(points[nextNode][0] - points[currNode][0]) + Math.abs(points[nextNode][1] - points[currNode][1]);
+
+                    minHeap.offer(new int[] { nextWeight, nextNode });
+                }
+            }
+        }
+
+        return cost;
+    }
+}
