0fcf3ef69e92e62ee002e7c1a9cab62d59d7d0db
ppa21 <47469086+ppa21@users.noreply.github.com>
2024-06-26 23:48:33 -0700

    Added a solution to Swim In Rising Water with time and space complexity

---
+++ src/778_SwimInRisingWater/Solution.java
@@ -0,0 +1,62 @@
+/*
+    * Time Complexity  = O((n^2)*log(n)); n = dimensions of the matrix
+    * Space Complexity = O(n^2)
+*/
+class Solution {
+    public int swimInWater(int[][] grid) {
+        
+        int[][] dirs = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } }; // Directions array to help move in the grid (up, down, left, right)
+        int length = grid.length;
+
+        // If the grid is 1x1, no need to swim
+        if (length == 1) {
+            return 0;
+        }
+
+        boolean[][] seen = new boolean[length][length]; // 'seen' array to keep track of visited cells
+        seen[0][0] = true;  // Mark the starting cell as visited
+
+        /*
+            * Min heap to determine the next cell to visit based on elevation
+            * Integer[] = (E, x, y); E = elevation at the current cell; x = x coordinate of the current cell; y = y coordinate of the current cell
+        */
+        PriorityQueue<Integer[]> minHeap = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        minHeap.add(new Integer[] { grid[0][0], 0, 0 });    // Add the starting cell to the min heap
+
+        // 'result' will store the minimum time to reach the end
+        int result = 0;
+
+        while (!minHeap.isEmpty()) {
+            Integer[] curr = minHeap.poll();
+
+            /* 
+                * Update 'result' with the maximum elevation we've encountered
+                * Compare current cell's elevation VS. result which stores max until reaching to the current cell
+            */
+            result = Math.max(result, curr[0]);
+
+            // If we've reached the bottom right cell, exit the loop
+            if (curr[1] == length - 1 && curr[2] == length - 1) {
+                break;
+            }
+
+            // Explore all possible directions from the current cell
+            for (int i = 0; i < 4; i++) {
+                int x = curr[1] + dirs[i][0];
+                int y = curr[2] + dirs[i][1];
+
+                // Check if the new cell is within bounds and not yet visited
+                if (x < 0 || x >= length || y < 0 || y >= length || seen[x][y]) {
+                    continue;
+                }
+
+                // Add the new cell to the min heap and mark it as visited
+                minHeap.add(new Integer[] { grid[x][y], x, y });
+                seen[x][y] = true;
+            }
+        }
+
+        // Return the minimum time required to reach the end
+        return result;
+    }
+}
