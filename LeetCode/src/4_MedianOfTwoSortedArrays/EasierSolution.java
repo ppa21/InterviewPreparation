8f2ebc60e10c7288747dfd4d9de86d66510fc5fb
ppa21 <47469086+ppa21@users.noreply.github.com>
2025-02-25 00:52:28 -0800

    Update EasierSolution.java

--- LeetCode/src/4_MedianOfTwoSortedArrays/EasierSolution.java
+++ LeetCode/src/4_MedianOfTwoSortedArrays/EasierSolution.java
@@ -4,8 +4,8 @@
 */
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        // Create a merged array with combined length
-        int[] mergedArray = new int[nums1.length + nums2.length];
+        int totalLength = nums1.length + nums2.length;
+        int[] mergedArray = new int[totalLength];
         
         int i = 0; // index for nums1
         int j = 0; // index for nums2
@@ -14,31 +14,39 @@ class Solution {
         // Merge the two sorted arrays
         while (i < nums1.length && j < nums2.length) {
             if (nums1[i] < nums2[j]) {
-                mergedArray[k++] = nums1[i++];
+                mergedArray[k] = nums1[i];
+                i++;
+                k++;
             } else {
-                mergedArray[k++] = nums2[j++];
+                mergedArray[k] = nums2[j];
+                j++;
+                k++;
             }
         }
         
         // Add remaining elements from nums1, if any
         while (i < nums1.length) {
-            mergedArray[k++] = nums1[i++];
+            mergedArray[k] = nums1[i];
+            i++;
+            k++;
         }
         
         // Add remaining elements from nums2, if any
         while (j < nums2.length) {
-            mergedArray[k++] = nums2[j++];
+            mergedArray[k] = nums2[j];
+            j++;
+            k++;
         }
         
         // Find the median of the merged array
-        int totalLength = mergedArray.length;
         if (totalLength % 2 != 0) {
             // Odd length: return the middle element
             return mergedArray[totalLength / 2];
         } else {
             // Even length: return the average of the two middle elements
-            return (mergedArray[totalLength / 2 - 1] + mergedArray[totalLength / 2]) / 2.0;
+            int mid1 = mergedArray[totalLength / 2 - 1];
+            int mid2 = mergedArray[totalLength / 2];
+            return (mid1 + mid2) / 2.0;
         }
     }
 }
-
