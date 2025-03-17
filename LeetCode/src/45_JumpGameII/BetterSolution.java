58550978a8e11a601881dad88d36a7959ea2b0c3
ppa21 <47469086+ppa21@users.noreply.github.com>
2025-03-17 03:21:32 -0700

    Update BetterSolution.java

--- LeetCode/src/45_JumpGameII/BetterSolution.java
+++ LeetCode/src/45_JumpGameII/BetterSolution.java
@@ -6,18 +6,18 @@ class Solution {
     public int jump(int[] nums) {
         // The starting range of the first jump is [0, 0]
         int answer = 0;
-        int curEnd = 0;
-        int curFar = 0;
+        int left = 0;
+        int right = 0;
 
         for (int i = 0; i < nums.length - 1; i++) {
             // Update the farthest reachable index of this jump.
-            curFar = Math.max(curFar, i + nums[i]);
+            right = Math.max(right, i + nums[i]);
 
             // If we finish the starting range of this jump,
             // Move on to the starting range of the next jump.
-            if (i == curEnd) {
+            if (i == left) {
                 answer++;
-                curEnd = curFar;
+                left = right;
             }
         }
 
