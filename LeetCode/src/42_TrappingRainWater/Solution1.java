f6ea38e12438fade4ea401d2ec62a3a2a85beb10
ppa21 <47469086+ppa21@users.noreply.github.com>
2025-03-24 19:26:21 -0700

    Update Solution1.java

--- LeetCode/src/42_TrappingRainWater/Solution1.java
+++ LeetCode/src/42_TrappingRainWater/Solution1.java
@@ -6,32 +6,34 @@ class Solution {
         if one side is smaller, that's the bottleneck;
         meaning that's the max height you can have;
         otherwise, the water will overflow;
-        hence, formula: Math.min(leftMax[i], rightMax[i]) - height[i]
+        hence, formula: Math.min(left[i], right[i]) - height[i]
         height   = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
-        leftMax  = [0, 0, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3]
-        rightMax = [3, 3, 3, 3, 3, 3, 3, 2, 2, 2, 1, 0]
+        left  = [0, 0, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3]
+        right = [3, 3, 3, 3, 3, 3, 3, 2, 2, 2, 1, 0]
     */
     public int trap(int[] height) {
-        int[] leftMax = new int[height.length];
-        int[] rightMax = new int[height.length];
+        public int trap(int[] height) {
+        int[] left = new int[height.length];
+        int[] right = new int[height.length];
 
         int max = height[0];
-        for (int i = 0; i < leftMax.length; i++) {
-            leftMax[i] = Math.max(height[i], max);
-            max = leftMax[i];
+        for (int i = 0; i < height.length; i++) {
+            left[i] = Math.max(height[i], max);        // MUST have max in Math.max() function
+            max = left[i];
         }
 
         max = height[height.length - 1];
-        for (int i = rightMax.length - 1; i >= 0; i--) {
-            rightMax[i] = Math.max(height[i], max);
-            max = rightMax[i];
+        for (int i = height.length - 1; i >= 0; i--) {
+            right[i] = Math.max(height[i], max);
+            max = right[i];
         }
 
         int result = 0;
         for (int i = 0; i < height.length; i++) {
-            result = result + Math.min(leftMax[i], rightMax[i]) - height[i];
+            result = result + Math.min(left[i], right[i]) - height[i];
         }
 
         return result;
+    }
     }
 }
