7187d06140603c01ad3b6b3ca10f1020fb4422fb
ppa21 <47469086+ppa21@users.noreply.github.com>
2025-07-09 23:28:27 -0700

    Update Solution.java

--- LeetCode/src/567_PermutationInString/Solution.java
+++ LeetCode/src/567_PermutationInString/Solution.java
@@ -1,7 +1,7 @@
 class Solution {
     /*
         * time complexity  = O(n)
-        * space complexity = O(n)
+        * space complexity = O(26) == O(1)
 
         use a sliding window of s1.length(); hence, size(SLIDING WINDOW) = s1.length();
             i = s1.length(); i < s2.length(); i++
@@ -17,21 +17,26 @@ class Solution {
             return true;
         }
 
+        // Arrays to count how many times each letter appears
         int[] freq1 = new int[26];
         int[] freq2 = new int[26];
 
+        // window is of size s1.length() because s1.length() <= s2.length()
+        // Setup: count letters in s1 AND first window of s2 (first window of s2 is s1.length())
         for (int i = 0; i < s1.length(); i++) {
             freq1[s1.charAt(i) - 'a']++;
             freq2[s2.charAt(i) - 'a']++;
         }
 
+        // window of s1.length() shifts by one (i++) to see if the new window in s2 makes it freq1 == freq2
         for (int i = s1.length(); i < s2.length(); i++) {
             if (Arrays.equals(freq1, freq2)) {
                 return true;
             }
 
-            freq2[s2.charAt(i - s1.length()) - 'a']--;
-            freq2[s2.charAt(i) - 'a']++;
+            // Slide window: remove leftmost char, add rightmost char
+            freq2[s2.charAt(i - s1.length()) - 'a']--;        // remove old
+            freq2[s2.charAt(i) - 'a']++;                      // add new
         }
 
         if (Arrays.equals(freq1, freq2)) {
