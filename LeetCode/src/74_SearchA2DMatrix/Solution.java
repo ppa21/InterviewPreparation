998dab7957534a13c1c2ef3c4ed35a601cfd46a2
ppa21 <47469086+ppa21@users.noreply.github.com>
2025-07-10 23:57:29 -0700

    Update Solution.java

--- LeetCode/src/74_SearchA2DMatrix/Solution.java
+++ LeetCode/src/74_SearchA2DMatrix/Solution.java
@@ -5,13 +5,13 @@ class Solution {
         * time complexity  = O(logmn)
         * space complexity = O(1)
 
-        matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]] -> [1, 3, 5, 7, 10, 11, 16, 20, 23, 30, 34, 60]
+        matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]] -> [1, 3, 5, 7, 10, 11, 16, 20, 23, 30, 34, 60] -> make 2d array into 1d array
     */
     public boolean searchMatrix(int[][] matrix, int target) {
         int rows = matrix.length; // 3
         int cols = matrix[0].length; // 4
         int left = 0;
-        int right = (rows * cols) - 1; // index 11
+        int right = (rows * cols) - 1; // index 11; make 2d array into 1d array
 
         while (left <= right) {
             int mid = left + (right - left) / 2;
