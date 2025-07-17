032a98e0319f17a651ca989fbdae350670184f5f
ppa21 <47469086+ppa21@users.noreply.github.com>
2025-07-17 16:47:58 -0700

    Create Variant1.java

---
+++ LeetCode/src/938_RangeSumOfBST/Variant1.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+ /*
+        * Time Complexity  = O(N)
+        * Space Complexity = O(N)
+
+        * Return the average of all the nodes within the lower and upper range
+ */
+class Solution {
+    public double rangeSumBST(TreeNode root, int low, int high) {
+        Stack<TreeNode> stack = new Stack<>();
+        double sum = 0;
+        double count = 0;
+
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+
+            root = stack.pop();
+
+            if (root.val >= low && root.val <= high) {
+                sum += root.val;
+                count++;
+            }
+
+            root = root.right;
+        }
+
+        return sum / count;
+    }
+}
