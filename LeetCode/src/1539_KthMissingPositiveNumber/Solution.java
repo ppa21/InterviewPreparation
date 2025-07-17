fb2106c3dd184603212bec683128080a3cf4932c
ppa21 <47469086+ppa21@users.noreply.github.com>
2025-07-17 15:00:53 -0700

    Create Solution.java

---
+++ LeetCode/src/1539_KthMissingPositiveNumber/Solution.java
@@ -0,0 +1,29 @@
+/*
+    * Time Complexity  = O(logn)
+    * Space Complexity = O(1)
+
+    * formula
+        * arr[middle] - 1 - middle
+        * count of missing numbers up until index middle
+*/
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int middle = left + (right - left) / 2;
+            int missing = arr[middle] - 1 - middle;
+
+            if (missing < k) {
+                left = middle + 1;
+            } else {
+                right = middle - 1;
+            }
+        }
+
+        // left = we've seen 'left' numbers from arr from left
+        // k    = we want kth missing number
+        return left + k;
+    }
+}
