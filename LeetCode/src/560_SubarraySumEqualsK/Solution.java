60faf8c6fae6e93bbe7a16eaeb98df923c9158bf
ppa21 <47469086+ppa21@users.noreply.github.com>
2025-07-17 22:32:03 -0700

    Create Solution.java

---
+++ LeetCode/src/560_SubarraySumEqualsK/Solution.java
@@ -0,0 +1,27 @@
+/*
+        * Time Complexity  = O(n);
+        * Space Complexity = O(n);
+*/
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int count = 0;
+        int currPrefixSum = 0;
+        Map<Integer, Integer> prefixSumToFreq = new HashMap<>();
+
+        // base case
+        prefixSumToFreq.put(0, 1);
+
+        for (int n : nums) {
+            currPrefixSum += n;
+
+            // currPrefixSum - k ----> look for previous sum that creates [currPrefixSum - k]
+            if (prefixSumToFreq.containsKey(currPrefixSum - k)) {
+                count += prefixSumToFreq.get(currPrefixSum - k);
+            }
+
+            prefixSumToFreq.put(currPrefixSum, prefixSumToFreq.getOrDefault(currPrefixSum, 0) + 1);
+        }
+
+        return count;
+    }
+}
