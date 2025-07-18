09533000a996eedff16d84c12d62f57d64314272
ppa21 <47469086+ppa21@users.noreply.github.com>
2025-07-18 00:29:25 -0700

    Create Variant1.java

---
+++ LeetCode/src/1249_MinimumRemovetoMakeValidParentheses/Variant1.java
@@ -0,0 +1,55 @@
+/*
+     * time complexity  = O(n)
+     * space complexity = O(n) - but slightly more space efficient than StringBuilder
+
+     * VARIANT: Space-optimized approach using char array. IN PLACE like c++ can't be done can't be done 
+ */
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+        int extraOpens = 0;
+        int totalOpens = 0;
+        char[] chars = s.toCharArray();
+        int j = 0;
+    
+        for (char ch : chars) {
+            if (ch == ')') {
+                if (extraOpens == 0) {
+                    continue;
+                }
+                
+                extraOpens--;
+                chars[j] = ch;
+                j++;
+            } else if (ch == '(') {
+                totalOpens++;
+                extraOpens++;
+                chars[j] = ch;
+                j++;
+            } else {
+                chars[j] = ch;
+                j++;
+            }
+        }
+    
+        int length = j;
+        j = 0;
+        int keep = totalOpens - extraOpens;
+    
+        for (int i = 0; i < length; i++) {
+            char ch = chars[i];
+            if (ch == '(') {
+                if (keep == 0) {
+                    continue;
+                }
+                chars[j] = ch;
+                j++;
+                keep--;
+            } else {
+                chars[j] = ch;
+                j++;
+            }
+        }
+    
+        return new String(chars, 0, j);
+    }
+}
