89b8b479473b89d87fe2d7c47446ca8f8a8e1095
ppa21 <47469086+ppa21@users.noreply.github.com>
2025-07-18 00:18:35 -0700

    Create Solution.java

---
+++ LeetCode/src/1249_MinimumRemovetoMakeValidParentheses/Solution.java
@@ -0,0 +1,46 @@
+/*
+        * time complexity  = O(n)
+        * space complexity = O(n)
+*/
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+        int extraOpens = 0;     
+        int totalOpens = 0;
+        StringBuilder temp = new StringBuilder();
+    
+        for (char ch : s.toCharArray()) {
+            if (ch == ')') {
+                if (extraOpens == 0) {
+                    continue;
+                }
+                
+                extraOpens--;
+                temp.append(ch);
+            } else if (ch == '(') {
+                totalOpens++;
+                extraOpens++;
+                temp.append(ch);
+            } else {
+                temp.append(ch);
+            }
+    }
+    
+        StringBuilder result = new StringBuilder();
+        int keep = totalOpens - extraOpens;
+    
+        for (char ch : temp.toString().toCharArray()) {
+            if (ch == '(') {
+                if (keep == 0) {
+                    continue;
+                }
+            
+            result.append(ch);
+            keep--;
+            } else {
+                result.append(ch);
+            }
+        }
+    
+        return result.toString();
+    }
+}
