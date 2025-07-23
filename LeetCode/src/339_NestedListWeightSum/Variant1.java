5ee5d11a94a4abac7cecb230b79becadc53c47fe
ppa21 <47469086+ppa21@users.noreply.github.com>
2025-07-22 19:01:58 -0700

    Create Variant1.java

---
+++ LeetCode/src/339_NestedListWeightSum/Variant1.java
@@ -0,0 +1,31 @@
+/*
+        * time complexity  = O(n); n = total numbers in the nested structure
+        * space complextiy = O(d); d = max depth
+
+        * VARIANT: What if you had to define your own schema for NestedList and implement DFS?
+*/
+class Solution {
+    public int depthSum(List<java.lang.Object> objs) {
+        return dfs(objs, 1);
+    }
+    
+    private int dfs(List<java.lang.Object> objs, int depth) {
+        int sum = 0;
+        for (java.lang.Object obj : objs) {
+            if (obj instanceof Integer) {
+                sum += (Integer) obj * depth;
+            } else {
+                sum += dfs(((Object) obj).value, depth + 1);
+            }
+        }
+        return sum;
+    }
+
+  class Object {
+        List<java.lang.Object> value;
+        
+        public Object() {
+            this.value = new ArrayList<>();
+        }
+    }
+}
