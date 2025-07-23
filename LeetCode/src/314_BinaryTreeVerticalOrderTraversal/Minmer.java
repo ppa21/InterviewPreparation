2eb20b8ba540ad7c9b45f6fbcfd790fb507d3d59
ppa21 <47469086+ppa21@users.noreply.github.com>
2025-07-22 17:11:04 -0700

    Create Minmer.java

---
+++ LeetCode/src/314_BinaryTreeVerticalOrderTraversal/Minmer.java
@@ -0,0 +1,71 @@
+/*
+        * time complexity  = O(n)
+        * space complexity = O(n)
+*/
+class Solution {
+    public List<List<Integer>> verticalOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        if (root == null) {
+            return result;
+        }
+
+        Map<Integer, List<Pair<TreeNode, Integer>>> colToNodeAndRow = new HashMap<>();
+        int[] bounds = levelOrderTraversal(root, colToNodeAndRow);
+
+        for (int i = bounds[0]; i <= bounds[1]; i++) {
+            // put sorting here if required i.e. 987. Vertical Order Traversal of a Binary Tree
+
+            List<Integer> currCol = new ArrayList<>();
+            for (Pair<TreeNode, Integer> pair : colToNodeAndRow.get(i)) {
+                currCol.add(pair.getKey().val);
+            }
+            result.add(currCol);
+        }
+
+        return result;
+    }
+
+    private int[] levelOrderTraversal(TreeNode root, Map<Integer, List<Pair<TreeNode, Integer>>> colToNodeAndRow) {
+        int leftmostCol = 0;
+        int rightmostCol = 0;
+        Queue<NodeMetadata> q = new LinkedList<>();
+        q.add(new NodeMetadata(0, 0, root));
+
+        while (!q.isEmpty()) {
+            NodeMetadata metadata = q.poll();
+            int row = metadata.row;
+            int col = metadata.col;
+            TreeNode node = metadata.node;
+
+            if (!colToNodeAndRow.containsKey(col)) {
+                colToNodeAndRow.put(col, new ArrayList<>());
+            }
+            colToNodeAndRow.get(col).add(new Pair<>(node, row));
+
+            leftmostCol = Math.min(leftmostCol, col);
+            rightmostCol = Math.max(rightmostCol, col);
+
+            if (node.left != null) {
+                q.add(new NodeMetadata(row + 1, col - 1, node.left));
+            }
+            if (node.right != null) {
+                q.add(new NodeMetadata(row + 1, col + 1, node.right));
+            }
+        }
+
+        return new int[]{leftmostCol, rightmostCol};
+    }
+
+    class NodeMetadata {
+        int row;
+        int col;
+        TreeNode node;
+
+        NodeMetadata(int row, int col, TreeNode node) {
+            this.row = row;
+            this.col = col;
+            this.node = node;
+        }
+    }
+}
