80713651a2e5d35382524866f037da3081151806
ppa21 <47469086+ppa21@users.noreply.github.com>
2025-07-29 00:26:58 -0700

    Update Variant1.java

--- LeetCode/src/346_MovingAveragefromDataStream/Variant1.java
+++ LeetCode/src/346_MovingAveragefromDataStream/Variant1.java
@@ -1,6 +1,6 @@
 /*
-        * time complexity  = O()
-        * space complexity = O()
+        * time complexity  = O(n)
+        * space complexity = O(n)
 
         * Variant: What if you had to return a resulting array of the averages of each subarray of size, "size"?
 */
@@ -12,12 +12,12 @@ public class SlidingWindowAverage {
         for (int right = 0; right < nums.length; right++) {
             windowSum += nums[right];
             
-            int left = right - windowSize;
+            int left = right - windowSize;                     // left is past the windowSize now
             if (left >= 0) {
                 windowSum -= nums[left];
             }
             
-            if (right >= windowSize - 1) {
+            if (right >= windowSize - 1) {                     // do we have windowSize elements to compute the average?
                 result.add((double) windowSum / windowSize);
             }
         }
