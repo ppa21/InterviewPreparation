43b351f1ee3914b38864692f8444f4d59c75038d
ppa21 <47469086+ppa21@users.noreply.github.com>
2025-07-28 22:25:23 -0700

    Update Variant1.java

--- LeetCode/src/1_TwoSum/Variant1.java
+++ LeetCode/src/1_TwoSum/Variant1.java
@@ -1,23 +1,23 @@
 /*
-		* time complexity  = O(n)
-  		* space complexity = O(n)
+	* time complexity  = O(n)
+  	* space complexity = O(n)
  
- 		* Variant: What if you had to return T/F if you find at least one pair of numbers that add up to the target?
+	* variant: What if you had to return T/F if you find at least one pair of numbers that add up to the target?
 */
 class Solution {
     public boolean twoSum_first_variant(int[] nums, int target) {
     	Set<Integer> complements = new HashSet<>();
 		
     	for (int n : nums) {
-			int complement = target - num;
+	    int complement = target - num;
 			
-        	if (complements.contains(complement)) {
-            	return true;
-        	}
+    	    if (complements.contains(complement)) {
+                return true;
+	    }
 			
-        	complements.add(n);
+            complements.add(n);
     	}
 		
     	return false;
-	}
+    }
 }
