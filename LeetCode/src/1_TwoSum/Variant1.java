0563be430727a0fbe2708596bef58b6a27a88559
ppa21 <47469086+ppa21@users.noreply.github.com>
2025-07-28 22:52:23 -0700

    Update Variant1.java

--- LeetCode/src/1_TwoSum/Variant1.java
+++ LeetCode/src/1_TwoSum/Variant1.java
@@ -5,7 +5,7 @@
 	* variant: what if you had to return T/F if you find at least one pair of numbers that add up to the target?
 */
 class Solution {
-    public boolean twoSum_first_variant(int[] nums, int target) {
+    public boolean twoSumFirstVariant(int[] nums, int target) {
     	Set<Integer> complements = new HashSet<>();
 		
     	for (int n : nums) {
