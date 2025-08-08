06b20311f0b9f6ebd27d0d04802d66ac7eb24215
ppa21 <47469086+ppa21@users.noreply.github.com>
2025-08-08 02:12:07 -0700

    Create Variant2.java

---
+++ LeetCode/src/21_MergeTwoSortedLists/Variant2.java
@@ -0,0 +1,42 @@
+/*
+        * time complexity  = O(n + m + p)
+        * space complexity = O(n + m + p)
+
+        * VARIANT: What if you had to merge 3 sorted integer lists into a resulting list without duplicates?
+*/
+class Solution {
+    public int[] mergeThreeSortedArrays(int[] arr1, int[] arr2, int[] arr3) {
+        List<Integer> list = new ArrayList<>();
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        
+        while(i < arr1.length || j < arr2.length || k < arr3.length) {
+            int val1 = (i < arr1.length) ? arr1[i] : Integer.MAX_VALUE;
+            int val2 = (j < arr2.length) ? arr2[j] : Integer.MAX_VALUE;
+            int val3 = (k < arr3.length) ? arr3[k] : Integer.MAX_VALUE;
+            
+            int minVal = Math.min(Math.min(val1, val2), val3);
+
+            if(list.isEmpty() || list.get(list.size() - 1) != minVal) { // not duplicate, then add
+                list.add(minVal);
+            }
+
+            list.add(minVal);
+            
+            if(val1 == minVal) {
+                i++;
+            } else if(val2 == minVal) {
+                j++;
+            } else {
+                k++;
+            }
+        }
+        
+        int[] result = new int[list.size()];
+        for(int idx = 0; idx < list.size(); idx++) {
+            result[idx] = list.get(idx);
+        }
+        return result;
+    }
+}
