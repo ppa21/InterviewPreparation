1b9ce6bb83efb23444d259c42962e5c72ff6c8dd
ppa21 <47469086+ppa21@users.noreply.github.com>
2025-08-19 17:36:53 -0700

    Create Solution.java

---
+++ LeetCode/src/1047_RemoveAllAdjacentDuplicatesInString/Solution.java
@@ -0,0 +1,17 @@
+/*
+        * Time Complexity:  O(n)
+        * Space Complexity: O(n)
+ */
+class Solution {
+    public String removeDuplicates(String s) {
+        StringBuilder result = new StringBuilder();
+        for (char ch : s.toCharArray()) {
+            if (result.length() == 0 || result.charAt(result.length() - 1) != ch) {
+                result.append(ch);
+            } else {
+                result.deleteCharAt(result.length() - 1);
+            }
+        }
+        return result.toString();
+    }
+}
