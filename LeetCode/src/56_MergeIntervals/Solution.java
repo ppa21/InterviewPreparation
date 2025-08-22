90dfe1a2748ca162eccd700e3d14f43c4886b892
ppa21 <47469086+ppa21@users.noreply.github.com>
2025-08-22 12:48:02 -0700

    Update Solution.java

--- LeetCode/src/56_MergeIntervals/Solution.java
+++ LeetCode/src/56_MergeIntervals/Solution.java
@@ -1,5 +1,26 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+
+        List<int[]> result = new ArrayList<>();
+        int[] last = intervals[0];
+        result.add(last);
+
+        for (int[] curr : intervals) {
+            if (last[1] >= curr[0]) {
+                last[1] = Math.max(last[1], curr[1]);
+            } else {
+                last = curr;
+                result.add(last);
+            }
+        }
+
+        return result.toArray(new int[result.size()][]);
+    }
+}
+
+class Solution {
+    public int[][] merge(int[][] intervals) {
         // intervals = [[1,3],[2,6],[8,10],[15,18]]
 
         // Sort by start time
