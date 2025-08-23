57f54165d3834d25587d76a7aeed38b46d3ca51c
ppa21 <47469086+ppa21@users.noreply.github.com>
2025-08-23 13:09:40 -0700

    Update Variant2.java

--- LeetCode/src/215_KthLargestElementInAnArray/Variant2.java
+++ LeetCode/src/215_KthLargestElementInAnArray/Variant2.java
@@ -5,7 +5,7 @@
     * VARIANT: What if you had to find the Kth smallest number in an integer array?
 */
 class Solution {
-    public int findKthLargest(int[] nums, int k) {
+    public int findKthSmallest(int[] nums, int k) {
         PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
 
         for (int n : nums) {
