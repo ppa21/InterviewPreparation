75140388a5b31a73ec93717f2664b9cb47a2d40f
ppa21 <47469086+ppa21@users.noreply.github.com>
2025-08-23 14:36:10 -0700

    Update Solution.java

--- LeetCode/src/21_MergeTwoSortedLists/Solution.java
+++ LeetCode/src/21_MergeTwoSortedLists/Solution.java
@@ -1,3 +1,30 @@
+/*
+        * time complexity  = O(n + m)
+        * space complexity = O(1)
+*/
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode result = new ListNode();
+        ListNode curr = result;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                curr.next = l1;
+                l1 = l1.next;
+            } else {
+                curr.next = l2;
+                l2 = l2.next;
+            }
+
+            curr = curr.next;
+        }
+
+        curr.next = l1 == null ? l2 : l1;
+
+        return result.next;
+    }
+}
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -110,30 +137,3 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-
-/*
-        * time complexity  = O(n + m)
-        * space complexity = O(1)
-*/
-class Solution {
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode result = new ListNode();
-        ListNode curr = result;
-
-        while (l1 != null && l2 != null) {
-            if (l1.val < l2.val) {
-                curr.next = l1;
-                l1 = l1.next;
-            } else {
-                curr.next = l2;
-                l2 = l2.next;
-            }
-
-            curr = curr.next;
-        }
-
-        curr.next = l1 == null ? l2 : l1;
-
-        return result.next;
-    }
-}
